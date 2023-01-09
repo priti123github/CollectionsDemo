eff206b7c5c75cabdc4eceb60d85c4e023d71877
pritipatil <ppatil5685@gmail.com>
2023-01-09 13:57:05 +0530

    added file for arraylist

---
+++ src/com/velocity/arraylist/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<String>();
+		list.add("priti");
+		list.add("patil");
+		list.add("pratiksha");
+		list.add("bhosale");
+		System.out.println(list);
+	}
+}
